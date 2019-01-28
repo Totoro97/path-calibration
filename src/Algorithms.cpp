2af84952c88d5ad703d92b82e5c0570d2eb49fb2
totoro97 <totoro97@outlook.com>
2019-01-28 19:05:19 +0800

    update

--- src/Algorithms.cpp
+++ src/Algorithms.cpp
@@ -11,16 +11,24 @@ void Algo::GetPathPoints(cv::Mat &mat, std::vector<Eigen::Vector2i> &path_2d) {
   int height = mat.rows;
   int width = mat.cols;
   if (mat.channels() != 1) {
-    std::cout << "Error" << std::endl;
+    std::cout << "Error: channels not eq 1" << std::endl;
     return;
   }
 
   uint8_t *data_ptr = mat.data;
   auto IsNeighbor = [height, width, data_ptr](int x, int y, int bias_x, int bias_y) {
+    int a = x + bias_x;
+    int b = y + bias_y;
+    if (a < 0 || a >= height || b < 0 || b >= width) {
+      return false;
+    }
+    if (data_ptr[a * width + b] != 255) {
+      return false;
+    }
     if (std::abs(bias_x) + std::abs(bias_y) <= 1)
       return true;
-    int a = x + bias_x;
-    int b = y;
+    a = x + bias_x;
+    b = y;
     if (a >= 0 && a < height && b >= 0 && b < width && data_ptr[a * width + b] == 255) {
       return false;
     }
@@ -36,6 +44,9 @@ void Algo::GetPathPoints(cv::Mat &mat, std::vector<Eigen::Vector2i> &path_2d) {
   int x_begin, y_begin;
   for (int i = 0; i < height; i++) {
     for (int j = 0; j < width; j++) {
+      if (data_ptr[i * width + j] != 255) {
+        continue;
+      }
       int du = 0;
       for (int a = -1; a <= 1; a++) {
         for (int b = -1; b <= 1; b++) {
@@ -47,7 +58,7 @@ void Algo::GetPathPoints(cv::Mat &mat, std::vector<Eigen::Vector2i> &path_2d) {
 
       // Debug
       if (du < 1) {
-        std::cout << "Error" << std::endl;
+        std::cout << "Error: Isolated Points." << std::endl;
         return;
       }
       if (du == 1) {
@@ -59,7 +70,7 @@ void Algo::GetPathPoints(cv::Mat &mat, std::vector<Eigen::Vector2i> &path_2d) {
   }
 
   if (num_ends != 2) {
-    std::cout << "Error" << std::endl;
+    std::cout << "Error: Not a Path: " << num_ends << "." << std::endl;
     return;
   }
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <opencv2/opencv.hpp>
 
 #include "Algorithms.h"
+#include "Thinning.h"
 
 void Run(std::string img_0_path, std::string img_1_path) {
   // Load Image
@@ -19,8 +20,11 @@ void Run(std::string img_0_path, std::string img_1_path) {
   cv::threshold(img_0_gray, img_0_gray, 10, 255, cv::THRESH_BINARY);
   cv::threshold(img_1_gray, img_1_gray, 10, 255, cv::THRESH_BINARY);
 
+  thinning(img_0_gray, img_0_gray);
+
   // Get path points (2D)
   std::vector<Eigen::Vector2i> path_2d;
+  cv::imwrite("out.png", img_0_gray);
   Algo::GetPathPoints(img_0_gray, path_2d);
 
   // Main algorithm
@@ -28,6 +32,6 @@ void Run(std::string img_0_path, std::string img_1_path) {
 }
 
 int main() {
-  Run("1.png", "2.png");
+  Run("0.png", "1.png");
   return 0;
 }
